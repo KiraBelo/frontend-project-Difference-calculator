f0fd1594669ec3148d7de55f70d39faa178539cb
KiraBelo <doeva2016@yandex.ru>
2022-10-07 18:20:27 +0300

    файл гендифф со справкой

---
+++ bin/gendiff.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+
+program
+.name('gendiff')
+.version('0.1.0', '-V --version', 'output the version number' )
+.helpOption('-h, --help', 'display help for command')
+.description('Compares two configuration files and show a difference');
+console.log("Ytlj");
+
+program.parse();
