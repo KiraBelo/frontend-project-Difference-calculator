cd08eb4ad84b93178df3308d66e271eb5fae562b
KiraBelo <doeva2016@yandex.ru>
2022-10-07 18:26:07 +0300

    -format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,7 @@ program
 .name('gendiff')
 .version('0.1.0', '-V --version', 'output the version number' )
 .helpOption('-h, --help', 'display help for command')
+.option('-f, --format <type>', 'output format')
 .description('Compares two configuration files and show a difference');
 console.log("Ytlj");
 
