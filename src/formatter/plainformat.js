4b44018625ba76f7e73fabebff15bef1b3941edf
KiraBelo <doeva2016@yandex.ru>
2023-02-04 22:06:18 +0300

    lint

--- src/formatter/plainformat.js
+++ src/formatter/plainformat.js
@@ -4,13 +4,13 @@ const formatPlain = (tree) => {
   const makeString = (data) => {
     return _.isObject(data) ? '[complex value]' : typeof data === 'string' ? `'${data}'` : String(data)
   }
-  
+
   const actions = {
     deleted: (allKeys) => `Property '${allKeys}' was removed`,
     added: (allKeys, value) => `Property '${allKeys}' was added with value: ${makeString(value)}`,
     changed: (allKeys, value1, value2) => `Property '${allKeys}' was updated. From ${makeString(value1)} to ${makeString(value2)}`,
     nested: (allKeys, children) => children.filter((child) => child !== '').join('\n'),
-    notchanged: () => '',
+    notchanged: () => ''
   }
 
   const iter = (data, parent) => {
@@ -18,7 +18,7 @@ const formatPlain = (tree) => {
     const allKeys = parent ? `${parent}.${key}` : `${key}`
     return actions[type](allKeys, value, value1, value2, children.map((child) => iter(child, allKeys)))
   }
- 
+
   return tree
     .map((data) => iter(data))
     .filter((data) => data !== '')
