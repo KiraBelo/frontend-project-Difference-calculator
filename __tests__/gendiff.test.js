ecf6ecc302f449f90f4133926d433d4a3fea6811
KiraBelo <doeva2016@yandex.ru>
2023-02-06 00:04:17 +0300

    fix

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,34 +1,71 @@
-import genDiff from '../src/index.js';
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'node:url';
+/* import gendiff from '../src/index.js';
+import path from 'node:path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { expect, test } from '@jest/globals';
 import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8').trim();
 
-const expectedJson = readFile('resultJson.txt').trim();
-const expectedPlain = readFile('resultPlain.txt').trim();
-const expectedStylish = readFile('resultStylish.txt').trim();
+const fileJson1 = readFile('file1.json');
+console.log(fileJson1);
+const fileJson2 = readFile('file2.json');
+const fileYml1 = readFile('file1.yml');
+const fileYml2 = readFile('file2.yml');
+
+const expectedJson = readFile('resultJson.txt');
+const expectedPlain = readFile('resultPlain.txt');
+const expectedStylish = readFile('resultStylish.txt');
 
 test('gendiff for .json', () => {
-  expect(genDiff('file1.json', 'file2.json', 'stylish')).toEqual(expectedStylish);
+  expect(gendiff(fileJson1, fileJson2, 'stylish')).toEqual(expectedStylish);
 });
 test('gendiff for .yml', () => {
-  expect(genDiff('file1.yml', 'file2.yml', 'plain')).toEqual(expectedPlain);
+  expect(gendiff(fileYml1, fileYml2, 'plain')).toEqual(expectedPlain);
 });
 test('gendiff for .json with plain format', () => {
-  expect(genDiff('file1.json', 'file2.json', 'plain')).toEqual(expectedPlain);
+  expect(gendiff(fileJson1, fileJson2, 'plain')).toEqual(expectedPlain);
 });
 test('gendiff for .yml with stylish format', () => {
-  expect(genDiff('file1.yml', 'file2.yml', 'stylish')).toEqual(expectedStylish);
+  expect(gendiff(fileYml1, fileYml2, 'stylish')).toEqual(expectedStylish);
 });
 
 test('gendiff for .json with json format', () => {
-  expect(genDiff('file1.json', 'file2.json', 'json')).toEqual(expectedJson);
+  expect(gendiff(fileJson1, fileJson2, 'json')).toEqual(expectedJson);
 });
 
 test('gendiff for .yml with json format', () => {
-  expect(genDiff('file1.yml', 'file2.yml', 'json')).toEqual(expectedJson);
+  expect(gendiff(fileYml1, fileYml2, 'json')).toEqual(expectedJson);
+}); */
+
+import { readFileSync } from 'fs';
+import gendiff from '../src/index.js';
+
+const outputPlain = readFileSync('__fixtures__/resultPlain.txt', 'utf8').trim();
+const outputStylish = readFileSync('__fixtures__/resultStylish.txt', 'utf8').trim();
+const outputJson = readFileSync('__fixtures__/resultJson.txt', 'utf8').trim();
+
+test('gendiff for .json', () => {
+  const path1 = '__fixtures__/file1.json';
+  const path2 = '__fixtures__/file2.json';
+  expect(gendiff(path1, path2, 'stylish')).toEqual(outputStylish);
+});
+test('gendiff for .yml', () => {
+  const path1 = '__fixtures__/file1.yml';
+  const path2 = '__fixtures__/file2.yml';
+  expect(gendiff(path1, path2, 'stylish')).toEqual(outputStylish);
+});
+test('gendiff for tree .json, plain format', () => {
+  const path1 = '__fixtures__/file1.json';
+  const path2 = '__fixtures__/file2.json';
+  expect(gendiff(path1, path2, 'plain')).toEqual(outputPlain);
+});
+
+test('gendiff for tree .json, json format', () => {
+  const path1 = '__fixtures__/file1.json';
+  const path2 = '__fixtures__/file2.json';
+  expect(gendiff(path1, path2, 'json')).toEqual(outputJson);
 });
--- src/index.js
+++ src/index.js
@@ -5,27 +5,25 @@ import parser from '../__fixtures__/parser.js';
 import getDifferences from './difference.js';
 import formatters from './formatter/index.js';
 
-/* const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename) */
 const getPath = (filepath) => path.resolve(process.cwd(), filepath);
-const getFormat = (filename) => path.extname(filename).slice(1);
+const getFormat = (filepath) => path.extname(filepath).slice(1);
 
-const gendiff = (filename1, filename2, format = 'stylish') => {
-  const format1 = getFormat(filename1);
-  const format2 = getFormat(filename2);
+const gendiff = (filepath1, filepath2, format = 'stylish') => {
+  const path1 = getPath(filepath1);
+  const path2 = getPath(filepath2);
 
-  const getFullPath1 = getPath(filename1);
-  const getFullPath2 = getPath(filename2);
+  const format1 = getFormat(filepath1);
+  const format2 = getFormat(filepath2);
 
-  const file1 = fs.readFileSync(getFullPath1, 'utf-8').trim();
-  const file2 = fs.readFileSync(getFullPath2, 'utf-8').trim();
+  const fileData1 = fs.readFileSync(path1, 'utf-8').trim();
+  const fileData2 = fs.readFileSync(path2, 'utf-8').trim();
 
-  const data1 = parser(file1, format1);
-  const data2 = parser(file2, format2);
+  const data1 = parser(fileData1, format1);
+  const data2 = parser(fileData2, format2);
   const diff = getDifferences(data1, data2);
+  const formattedDiffs = formatters(diff, format);
 
-  return formatters(diff, format);
+  return formattedDiffs;
 };
 
 export default gendiff;
