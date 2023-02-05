7428a54a4ef98e40bb53e78f5dc2169402a4ae39
KiraBelo <doeva2016@yandex.ru>
2023-02-05 21:29:05 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -6,15 +6,14 @@ import { fileURLToPath } from 'node:url'
 import getDifferences from './difference.js'
 import formatters from './formatter/index.js'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename)
+const getFormat = (filename) => path.extname(filename).slice(1)
+
 const gendiff = (filename1, filename2, format = 'stylish') => {
-  const extractFormat = (filename) => path.extname(filename).slice(1)
-
-  const extention1 = extractFormat(filename1)
-  const extention2 = extractFormat(filename2)
-
-  const __filename = fileURLToPath(import.meta.url)
-  const __dirname = dirname(__filename)
-  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename)
+  const format1 = getFormat(filename1)
+  const format2 = getFormat(filename2)
 
   const getFullPath1 = getFixturePath(filename1)
   const getFullPath2 = getFixturePath(filename2)
@@ -22,8 +21,8 @@ const gendiff = (filename1, filename2, format = 'stylish') => {
   const file1 = fs.readFileSync(getFullPath1, 'utf-8').trim()
   const file2 = fs.readFileSync(getFullPath2, 'utf-8').trim()
 
-  const data1 = parser(file1, extention1)
-  const data2 = parser(file2, extention2)
+  const data1 = parser(file1, format1)
+  const data2 = parser(file2, format2)
   const diff = getDifferences(data1, data2)
 
   return formatters(diff, format)
