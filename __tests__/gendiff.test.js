8f4fbccf91481af482798cedf79e87a82c5d9f14
KiraBelo <doeva2016@yandex.ru>
2023-02-16 15:34:39 +0300

    linter

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -13,23 +13,22 @@ const outputStylish = readFile('resultStylish.txt').trim();
 const outputJson = readFile('resultJson.txt').trim();
 
 describe('difference calculator', () => {
-test ('yml', () =>{
-expect(gendiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'stylish')).toEqual(outputStylish);
-expect(gendiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'plain')).toEqual(outputPlain);
-expect(gendiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'json')).toEqual(outputJson);
-expect(gendiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toEqual(outputStylish);
-})
-test ('yaml', () =>{
-expect(gendiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), 'stylish')).toEqual(outputStylish);
-expect(gendiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), 'plain')).toEqual(outputPlain);
-expect(gendiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), 'json')).toEqual(outputJson);
-expect(gendiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'))).toEqual(outputStylish);
-})
-test ('json', () =>{
-expect(gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish')).toEqual(outputStylish);
-expect(gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toEqual(outputPlain);
-expect(gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json')).toEqual(outputJson);
-expect(gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(outputStylish);
+  test('yml', () => {
+    expect(gendiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'stylish')).toEqual(outputStylish);
+    expect(gendiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'plain')).toEqual(outputPlain);
+    expect(gendiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'json')).toEqual(outputJson);
+    expect(gendiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toEqual(outputStylish);
+  });
+  test('yaml', () => {
+    expect(gendiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), 'stylish')).toEqual(outputStylish);
+    expect(gendiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), 'plain')).toEqual(outputPlain);
+    expect(gendiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), 'json')).toEqual(outputJson);
+    expect(gendiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'))).toEqual(outputStylish);
+  });
+  test('json', () => {
+    expect(gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish')).toEqual(outputStylish);
+    expect(gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toEqual(outputPlain);
+    expect(gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json')).toEqual(outputJson);
+    expect(gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(outputStylish);
+  });
 });
-});
-
