3ca7e3a8f0c1d543e49d32e76099659e5a4eb736
KiraBelo <doeva2016@yandex.ru>
2023-03-06 14:12:49 +0300

    fix#3

--- src/formatter/stylish.js
+++ src/formatter/stylish.js
@@ -8,29 +8,26 @@ const stringify = (node, depth = 1) => {
   const lines = Object.entries(node).map(([key, value]) => `${makeSpaces(depth + 1)}  ${key}: ${stringify(value, depth + 1)}`);
   return `{\n${[...lines].join('\n')}\n${makeSpaces(depth)}  }`;
 };
-const iter = (node, depth) => {
-  const types = node.type;
-  const keys = node.key;
-
-  switch (types) {
+const iter = (tree, depth = 1) => tree.map((node) => {
+  switch (node.type) {
     case 'nested': {
-      const lines = node.children.map((child) => iter(child, depth + 1));
-      return `${makeSpaces(depth)}  ${keys}: {\n${lines.join('\n')}\n${makeSpaces(depth)}  }`;
+      const lines = iter(node.children, depth + 1);
+      return `${makeSpaces(depth)}  ${node.key}: {\n${lines.join('\n')}\n${makeSpaces(depth)}  }`;
     }
-    case 'deleted': return `${makeSpaces(depth)}- ${keys}: ${stringify(node.value, depth)}`;
+    case 'deleted': return `${makeSpaces(depth)}- ${node.key}: ${stringify(node.value, depth)}`;
     case 'changed': {
-      const line1 = `${makeSpaces(depth)}- ${keys}: ${stringify(node.value1, depth)}`;
-      const line2 = `${makeSpaces(depth)}+ ${keys}: ${stringify(node.value2, depth)}`;
+      const line1 = `${makeSpaces(depth)}- ${node.key}: ${stringify(node.value1, depth)}`;
+      const line2 = `${makeSpaces(depth)}+ ${node.key}: ${stringify(node.value2, depth)}`;
       return `${line1}\n${line2}`;
     }
-    case 'added': return `${makeSpaces(depth)}+ ${keys}: ${stringify(node.value, depth)}`;
-    case 'notchanged': return `${makeSpaces(depth)}  ${keys}: ${stringify(node.value, depth)}`;
+    case 'added': return `${makeSpaces(depth)}+ ${node.key}: ${stringify(node.value, depth)}`;
+    case 'notchanged': return `${makeSpaces(depth)}  ${node.key}: ${stringify(node.value, depth)}`;
     default: return 'error: unknown type';
   }
-};
+});
 
 const formaterStylish = (tree) => {
-  const result = tree.map((node) => iter(node, 1));
+  const result = iter(tree, 1);
   return `{\n${[...result].join('\n')}\n}`;
 };
 export default formaterStylish;
